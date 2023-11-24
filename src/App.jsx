32c213150d4360b67d7eb3130e08fdf5dda25a20
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-11-25 02:10:08 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  let [count, setCount] = useState(51)
+
+  let increment = () => {
+    if(count < 100){
+    setCount(count + 1);
+  }
+  }
+  let decrement = () => {
+    if(count > 0){
+    setCount(count - 1);
+    }
+  }
 
   return (
     <>
-      
+      <h1>ReactJs Counter Application | Vite | Limit 0 to 100</h1>
+      <h3>Current Value: {count}</h3>
+      <button onClick={increment}>Increment Value</button>
+      <button onClick={decrement}>Decrement Value</button>
+      <p>Designed By Vivek Anand</p>
     </>
   )
 }
